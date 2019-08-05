53796b731ff2f3745ddedc92b3cfb21e972f308a
ravitejadakoji <53676429+ravitejadakoji@users.noreply.github.com>
2019-08-05 11:55:22 +0530

    Create adc.h

---
+++ adc.h
@@ -0,0 +1,17 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include <avr/io.h>
+#include "stdutils.h"
+
+
+/************************
+ ADC Function Prototypes
+ ************************/
+
+void ADC_Init();
+uint16_t ADC_GetADCValue(uint8_t var_adcChann);
+
+
+
+#endif
